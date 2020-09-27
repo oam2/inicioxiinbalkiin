27ba478b41cde935d311d797b6f17330cad99cf7
oam2 <orlando57m2@gmail.com>
2020-09-26 21:43:54 -0500

    botondNavegationrawer

--- src/components/BarraNavegacion.vue
+++ src/components/BarraNavegacion.vue
@@ -1,58 +1,35 @@
 <template>
-     <div>
-    <v-app-bar
-      color="deep-purple accent-4"
-      dense
-      dark
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
+  <div>
+    <v-app-bar color="deep-purple accent-4" dense dark>
+      <v-app-bar-nav-icon  @click ="mostrarNavegationDrawer = true">
+       
+      </v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
       <v-toolbar-title>Xiinbalkiin</v-toolbar-title>
 
       <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-menu
-        left
-        bottom
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            icon
-            v-bind="attrs"
-            v-on="on"
-          >
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item
-            v-for="n in 5"
-            :key="n"
-            @click="() => {}"
-          >
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
+      <b-btn icon>
+        <v-icon> mdi-google-translate </v-icon>
+        ES
+      </b-btn>
     </v-app-bar>
+    <NavegationDrawer
+      :drawer="mostrarNavegationDrawer"
+      @cerrar="mostrarNavegationDrawer = false"
+    />
   </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import NavegationDrawer from "./NavegationDrawer.vue";
+
+export default {
+  components: {
+    NavegationDrawer,
+  },
+  data: () => ({ mostrarNavegationDrawer: false }),
+};
 </script>
 
 <style lang="scss" scoped>
-
-</style>
+</style> 
