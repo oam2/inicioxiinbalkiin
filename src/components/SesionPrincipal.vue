121ac3fd830c10bfd5d4d1e6dd48322f546547dc
oam2 <orlando57m2@gmail.com>
2020-10-03 01:57:54 -0500

    implementaciondetextoe imagen

---
+++ src/components/SesionPrincipal.vue
@@ -0,0 +1,26 @@
+<template> 
+ <div class="grey lighten-5">
+     
+  <v-container>
+      <v-row>
+          <v-col cols="6" class="pa-0 ma-0">
+              <v-img src="@/assets/sesionprincipal.jpg"></v-img>
+          </v-col>
+          <v-col cols="6">
+              <h1 class ="text-center">XiinBalkiin</h1>
+              <h2 class ="text-center">Sistema de transporte publico</h2>
+          </v-col>
+      </v-row>
+  </v-container>
+ </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
 <div class="home">
     <BarraNavegacion />
     <Mapa />
+    <Sesion/>
 </div>
 </template>
 
 <script>
 import BarraNavegacion from "../components/BarraNavegacion";
 import Mapa from "../components/Mapa";
+import Sesion from "../components/SesionPrincipal";
 export default {
     name: 'Home',
     components: {
         BarraNavegacion,
-        Mapa
+        Mapa,
+        Sesion
 
     }
 }
